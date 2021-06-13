bbf94829949ea68a4819ed38087e78cfd1dfa828
adar2378 <saifulislamadar@gmail.com>
2021-06-13 09:23:49 +0600

    added exception

--- lib/repositories/tweets/tweets_repository.dart
+++ lib/repositories/tweets/tweets_repository.dart
@@ -1,32 +1,54 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:twitterapp/models/tweet.dart';
 
+class TweetException implements Exception {
+  final String? message;
+
+  TweetException(this.message);
+}
+
 class TweetRepository {
   TweetRepository(this._firebaseFirestore);
   final FirebaseFirestore _firebaseFirestore;
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getTweetsStream() {
-    final stream = _firebaseFirestore
-        .collection('tweets')
-        .orderBy('dateCreated', descending: true)
-        .snapshots();
-    return stream;
+    try {
+      final stream = _firebaseFirestore
+          .collection('tweets')
+          .orderBy('dateCreated', descending: true)
+          .snapshots();
+      return stream;
+    } catch (e, st) {
+      throw (TweetException('Error: $e, Stacktrace: $st'));
+    }
   }
 
   Future<void> addTweet(Tweet tweet) async {
-    await _firebaseFirestore
-        .collection('tweets')
-        .add(tweet.toMap().remove('id'));
+    try {
+      await _firebaseFirestore
+          .collection('tweets')
+          .add(tweet.toMap().remove('id'));
+    } catch (e, st) {
+      throw (TweetException('Error: $e, Stacktrace: $st'));
+    }
   }
 
   Future<void> editTweet(Tweet tweet) async {
-    await _firebaseFirestore
-        .collection('tweets')
-        .doc(tweet.id)
-        .update(tweet.toMap().remove('id'));
+    try {
+      await _firebaseFirestore
+          .collection('tweets')
+          .doc(tweet.id)
+          .update(tweet.toMap().remove('id'));
+    } catch (e, st) {
+      throw (TweetException('Error: $e, Stacktrace: $st'));
+    }
   }
 
   Future<void> deleteTweet(String id) async {
-    await _firebaseFirestore.collection('tweets').doc(id).delete();
+    try {
+      await _firebaseFirestore.collection('tweets').doc(id).delete();
+    } catch (e, st) {
+      throw (TweetException('Error: $e, Stacktrace: $st'));
+    }
   }
 }
